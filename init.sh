9438a8bbc96c8d926b4b1fed5a7eba4a7ac543f7
batfink <vnystad@gmail.com>
2019-04-22 16:26:18 +0200

    update script

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git submodule init
 git submodule update
-git submodule foreach git checkout starter
-git submodule foreach git pull --rebase origin starter
+git submodule foreach git checkout master
+git submodule foreach git pull --rebase origin master
